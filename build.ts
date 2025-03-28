9ec33073a91876bfdae415ff92787a38abe359e3
unknown <farismmk@gmail.com>
2025-03-28 03:14:15 -0700

    use xbuild

---
+++ build.ts
@@ -0,0 +1,37 @@
+import { type Build } from 'xbuild';
+
+const build: Build = {
+    common: {
+        project: 'tinyregexc',
+        archs: ['x64'],
+        variables: [],
+        copy: {},
+        defines: [],
+        options: [],
+        subdirectories: [],
+        libraries: {
+            tinyregexc: {
+                sources: ['tiny-regex-c/re.c']
+            }
+        },
+        buildDir: 'build',
+        buildOutDir: 'libs',
+        buildFlags: []
+    },
+    platforms: {
+        win32: {
+            windows: {},
+            android: {
+                archs: ['x86', 'x86_64', 'armeabi-v7a', 'arm64-v8a'],
+            }
+        },
+        linux: {
+            linux: {}
+        },
+        darwin: {
+            macos: {}
+        }
+    }
+}
+
+export default build;
--- build_libs.bat
+++
@@ -1,9 +0,0 @@
-mkdir libs
-mkdir libs\debug
-mkdir libs\release
-
-clang -c -g -gcodeview -o tinyregexc.lib -target x86_64-pc-windows -fuse-ld=llvm-lib -Wall tiny-regex-c/re.c
-move tinyregexc.lib libs\debug
-
-clang -c -o tinyregexc.lib -target x86_64-pc-windows -fuse-ld=llvm-lib -Wall tiny-regex-c/re.c
-move tinyregexc.lib libs\release
